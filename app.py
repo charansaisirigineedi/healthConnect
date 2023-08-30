f16596d710545d97ad444d273bf87cac5ab0f9fd
charanmcr <charan_mcr@srkrec.edu.in>
2023-08-31 01:51:03 +0530

    Hospital Module Addition

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from blueprints.blog.blog import blog
 from blueprints.doctor.doctor import doctor
+from blueprints.hospital.hospital import hospital
 from blueprints.lab.lab import lab
 from blueprints.user.user import user
 
@@ -11,6 +12,7 @@ app.register_blueprint(user)
 app.register_blueprint(doctor)
 app.register_blueprint(lab)
 app.register_blueprint(blog)
+app.register_blueprint(hospital)
 
 UPLOAD_FOLDER = os.path.join(app.root_path,'static','uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
